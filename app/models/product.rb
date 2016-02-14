c6704f4bdacc3f0b5c477a941e87b898e4e6a82d
icdozen <icdozen@gmail.com>
2016-02-14 09:55:49 -0500

    first commit

---
+++ app/models/product.rb
@@ -0,0 +1,10 @@
+class Product < ActiveRecord::Base
+  validates :description, :name, presence: true
+  validates :price_in_dollars, numericality: {only_integer: true, greater_than: 0}
+  has_many :reviews
+
+  def formatted_price
+    pdolla = price_in_dollars.to_f 
+    format("%.2f", pdolla)
+  end
+end
